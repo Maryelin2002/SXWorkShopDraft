70f3ad8cf43bf10850f7358a6fa193110a15416e
Yelincode <yelincode@gmail.com>
2021-08-18 12:09:01 -0400

    add files

---
+++ SolvexWorkShopClient-main/src/router/user.router.ts
@@ -0,0 +1,22 @@
+import { RouteConfig } from "vue-router";
+const item = {
+  path: "/admin/users",
+  name:"Users",
+  component: () => import("../views/base.vue"),
+  children: [
+    {
+      path: "",
+      component: () => import("../views/admin/users/list.vue"),
+    },
+    {
+      path: "add",
+      component: () => import("../views/admin/users/add.vue")
+    },
+    {
+        path: "edit/:id",
+        component: () => import("../views/admin/users/edit.vue")
+      }
+  ],
+} as RouteConfig;
+
+export default item;
